c600f5ae1e271c516a0f1132e03c14183a73d506
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-06-19 20:29:16 +0800

    add: admin/noauth.php date info on staff login success.

--- admin/noauth.php
+++ admin/noauth.php
@@ -9,7 +9,9 @@ $tab = "admin";
 $nav = "";
 
 Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
+date_default_timezone_set('Asia/Manila'); // Philippine timezone
 
-echo '<p class="error">'.T("adminNoauth").'</p>';
+echo '<p class="error">' . T("adminNoauth") . '</p>';
+echo '<h3>Welcome to BurauenBiblio! Current Date and Time: <b>' . date("m-d-Y H:i:s") . '</b></h3>' ;
 
- ;
+ 
