7abb5951d10b0ce8c0f0842da178011caa6ac495
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-06-19 20:22:15 +0800

    add: date info on welcome after successful login.

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,9 @@ Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
 
     <h1><img src="../images/admin.png" border="0" width="30" height="30" align="top"> <?php echo T("Admin") ?></h1>
     <fieldset> <?php echo T("adminIndexDesc"); ?> </fieldset>
-
+		<h3><?php  
+			date_default_timezone_set('Asia/Manila'); // Philippine timezone
+			echo "Welcome to BurauenBiblio! Current Date and Time: <b>" . date("m-d-Y H:i:s") . "</b>";
+		?> </h3>
 <?php
     ReportDisplaysUI::display('admin');
