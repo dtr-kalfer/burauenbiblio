210e61b7652388f3982162be57ef09497d7d56cc
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-06-26 20:47:39 +0800

    refactor: catalog/print_card_catalog.php, improved UI and prepare to fix error if bibID not found.

--- catalog/print_card_catalog.php
+++ catalog/print_card_catalog.php
@@ -1,38 +1,49 @@
 <?php
-
-	require_once("../shared/common.php");
-	$tab = "cataloging";
-	$nav = "print_card_catalog";	
-	require_once(REL(__FILE__, "../shared/logincheck.php"));
-	require_once(REL(__FILE__, "../functions/inputFuncs.php"));
-	Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
+require_once("../shared/common.php");
+$tab = "cataloging";
+$nav = "print_card_catalog";	
+require_once(REL(__FILE__, "../shared/logincheck.php"));
+require_once(REL(__FILE__, "../functions/inputFuncs.php"));
+Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
 ?>
-	<h3><?php echo T("Print Card Catalog"); ?></h3>
-	<?php //print_r($_SESSION); // for debugging ?>
-	<div id="crntMbrDiv_new">To be filled by server --F.T.</div>
-	<p id="errSpace" class="error">to be filled by server</p>
-	<!-- ------------------------------------------------------------------------ -->
-	<div id="bulkDel_formDiv" style="display: block;">
-      <fieldset>
-          <p>To get better result similar to example, use space between call number <br>
-					i.e. FIL 332.10959 C11 2020 and also fill up other details on biblio records.</p>
-          <br />
-          <!-- -------------------------->
-						<form name="print_bibid" method="post" action="print_bibid.php">
-						<p>Each author catalog results to 1/2 Letter page (Portrait), to complete 
-						entire page,<br> use <b>TWO</b> bibid entries</p><br>
-						<p><b>Enter Bibid ID# below:</b></p><br>
-						<input type="text"  oninput="this.value = this.value.replace(/\D/g, '').slice(0, 5)" pattern="\d*" name="bibid_fpdf" value="" id="" maxlength="5" required />
-						<input type="text"  oninput="this.value = this.value.replace(/\D/g, '').slice(0, 5)" pattern="\d*" name="bibid_fpdf2" value="" id="" maxlength="5" required />
-						<input type="submit" value=" Print Bibid ID " name="print_bibid" onclick="return confirm('Print Bibid ID?');"/>
-						</form>
-					<img src="../images/card_catalog_demo_sample.webp" alt="sample card catalog" />
-					<!-- -------------------------->
-      </fieldset>
-	</div>
-	<!-- ------------------------------------------------------------------------ -->
+<!-- HTMX script (local or CDN) -->
+<script src="../htmx_cdn/htmx.min.js"></script>
+
+<h3><?php echo T("Print Card Catalog"); ?></h3>
+
+<h3 id="crntMbrDiv_new" style="background-color: skyblue;" >...</h3>
+<p id="errSpace" class="error"></p>
+
+<div id="bulkDel_formDiv" style="display: block;">
+  <fieldset>
+    <p>To get better result similar to example, use space between call number <br>
+    i.e. FIL 332.10959 C11 2020 and also fill up other details on biblio records.</p><br>
+
+    <form name="print_bibid" method="post" 
+          hx-post="print_bibid.php"
+          hx-target="#crntMbrDiv_new"
+          hx-swap="innerHTML"
+          onsubmit="return confirm('Print Bibid ID?');">
+
+      <p><b>Enter two BibID below:</b></p><br>
+			<label for="bibid_fpdf">bibID #1</label>
+      <input type="text" name="bibid_fpdf"
+             oninput="this.value = this.value.replace(/\D/g, '').slice(0, 5)"
+             pattern="\d*" maxlength="5" required />
+			<label for="bibid_fpdf2">bibID #2</label>
+      <input type="text" name="bibid_fpdf2"
+             oninput="this.value = this.value.replace(/\D/g, '').slice(0, 5)"
+             pattern="\d*" maxlength="5" required />
+
+      <input type="submit" value="Print Bibid ID" />
+    </form>
+		<h3>Sample Printout</i></h3>
+    <img src="../images/card_catalog_demo_sample.webp" alt="sample card catalog" />
+  </fieldset>
+</div>
+
 <?php
-  require_once(REL(__FILE__,'../shared/footer.php'));
-?>	
+require_once(REL(__FILE__, '../shared/footer.php'));
+?>
 </body>
 </html>
