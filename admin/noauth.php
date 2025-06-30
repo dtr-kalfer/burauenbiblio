efd222c3a38ca475337a8b013e3da3bc639063e2
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-06-30 23:12:04 +0800

    add improvement: added jsCalendar on the welcome noauth page. this will still be improved with added features in future.

--- admin/noauth.php
+++ admin/noauth.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="../jscalendar/jsCalendar.min.css">
+<script type="text/javascript" src="../jscalendar/jsCalendar.min.js"></script>
 <?php
 /* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
  * See the file COPYRIGHT.html for more details.
@@ -5,12 +7,17 @@
 
 require_once("../shared/common.php");
 
-$tab = "admin";
-$nav = "";
+	$tab = "circulation";
+	$nav = "";
 
 Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
 date_default_timezone_set('Asia/Manila'); // Philippine timezone
 
-echo '<p class="error">' . T("adminNoauth") . '</p>';
-echo '<h3>Welcome to BurauenBiblio! Current Date and Time: <b>' . date("m-d-Y H:i:s") . '</b></h3>' ;
- 
+echo '<h3>Welcome to BurauenBiblio!</h3>';
+echo '<p class="info" style="text-align: center;">' . $_SESSION["username"] . T(", you are now logged in.") . '</p>';
+echo '<p>' . T("showstaffwelcome") . '</p>';
+
+?>
+
+<section class="auto-jsCalendar classic-theme red" data-month-format="month YYYY" style='padding: 10px; margin: auto; width: 81%;'></section>
+<h3><?php echo T("Today is: ") . date("m-d-Y"); ?></h3>
