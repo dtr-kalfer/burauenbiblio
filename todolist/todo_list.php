e0c8317504d556e00c4e14cf650755fdbc52f482
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-05 22:12:05 +0800

    update: include guard_token.php into the todo_list.php

--- todolist/todo_list.php
+++ todolist/todo_list.php
@@ -1,4 +1,6 @@
 <?php
+require_once("../shared/guard_token.php"); 
+verify_token_or_die('guard_token_key'); // your custom key
 
 		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
 		 * See the file COPYRIGHT.html for more details. --F.Tumulak
