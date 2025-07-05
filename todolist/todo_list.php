d6b368deb2b0b3d0b64268eb4671d22596c104dd
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-05 22:09:53 +0800

    added todo_list.php using htmx hx-post hx-vals.. etc. also include PDO for sqlite.

---
+++ todolist/todo_list.php
@@ -0,0 +1,38 @@
+<?php
+
+		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+		 * See the file COPYRIGHT.html for more details. --F.Tumulak
+		 */
+		 
+require '../todolist/db.php';
+$stmt = $db->query("SELECT * FROM todos ORDER BY id ASC");
+$todos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<?php foreach ($todos as $todo): ?>
+  <li class="<?= $todo['is_done'] ? 'done' : '' ?>">
+    <input type="checkbox"
+      <?= $todo['is_done'] ? 'checked' : '' ?>
+      hx-post="../todolist/todo_update.php"
+      hx-vals='{
+        "id": <?= $todo['id'] ?>,
+        "done": <?= $todo['is_done'] ? 'false' : 'true' ?>,
+        "guard_token_key": "<?= $token ?>"
+      }'
+      hx-target="#todo-list"
+      hx-swap="innerHTML"
+    >
+    <?= htmlspecialchars($todo['task']) ?>
+    <button
+      hx-post="../todolist/todo_delete.php"
+      hx-vals='{
+        "id": <?= $todo['id'] ?>,
+        "guard_token_key": "<?= $token ?>"
+      }'
+      hx-target="#todo-list"
+      hx-swap="innerHTML"
+    >🗑</button>
+  </li>
+<?php endforeach; ?>
+
