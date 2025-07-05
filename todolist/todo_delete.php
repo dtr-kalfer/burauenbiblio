8c163015f8dcc10e07b258f248bf802c2bcd9042
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-05 22:24:26 +0800

    add todolist/todo_delete for htmx use

---
+++ todolist/todo_delete.php
@@ -0,0 +1,15 @@
+<?php
+		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+		 * See the file COPYRIGHT.html for more details. --F.Tumulak
+		 */
+require '../todolist/db.php';
+
+if (!empty($_POST['todo'])) {
+    $stmt = $db->prepare("INSERT INTO todos (task) VALUES (:task)");
+    $stmt->bindValue(':task', $_POST['todo'], PDO::PARAM_STR);
+    $stmt->execute();
+}
+
+echo '<div id="todo-list">';
+include '../todolist/todo_list.php';
+echo '</div>';
