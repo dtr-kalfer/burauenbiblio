6d6dcbf65213deb0c46cf86a58682e13f5f4c674
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-07 14:02:26 +0800

    refactor: classes/ObServer.php update check_hmac function

--- classes/ObServer.php
+++ classes/ObServer.php
@@ -2,47 +2,38 @@
 require_once("../shared/common.php");
 require_once("../model/Staff.php");
 
-class ObServer {
-        public static function check_hmac() {
-            $headers = ObServer::get_headers();
-                if(isset($headers['Authcheck'])){
-                        $matches = array();
-                        preg_match('/Token token="(.*)"/', $headers['Authcheck'], $matches);
-                        if(isset($matches[1])){
-                            $token = $matches[1];
-                        } else {
-                            return 0;
-                        }
-                }
-                if(isset($headers['authcheck'])){
-                        $matches = array();
-                        preg_match('/Token token="(.*)"/', $headers['authcheck'], $matches);
-                        if(isset($matches[1])){
-                            $token = $matches[1];
-                        } else {
-                            return 0;
-                        }
-                }
-                if (Settings::get('hmac_timeout')) {
-                        $earliestLegitSendTime = time() - (Settings::get('hmac_timeout') * 60);
-                } else {
-                        $earliestLegitSendTime = time() - (28 * 24 * 60 * 60); // Four weeks ago
-                }
-                if (!isset($_POST['timestamp'])) {
-                        return 0;
-                }
-                if ($earliestLegitSendTime > ($_POST['timestamp'])) {
-                        return 0;
-                }
-                $requestor = new Staff;
-                $rows = $requestor->getMatches(array('username'=>$_POST['username']));
-								$expected_hash = null; // Prevent undefined variable warning
-                foreach ($rows as $row) {
-                        $expected_hash = hash_hmac('md5', $_POST['mode'].'-'.$row['username'].'-'.$_POST['timestamp'], $row['secret_key']);
-                }
-                return ($expected_hash === $token);
-        }
+	class ObServer {
+	public static function check_hmac() {
+			$headers = array_change_key_case(ObServer::get_headers(), CASE_LOWER);
+			$token = null;
 
+			if (isset($headers['authcheck'])) {
+					if (preg_match('/Token token="(.+?)"/', $headers['authcheck'], $matches)) {
+							$token = $matches[1];
+					} else {
+							return 0;
+					}
+			} else {
+					return 0;
+			}
+
+			$timeout = Settings::get('hmac_timeout') ?: (28 * 24 * 60); // default: 4 weeks
+			$earliestLegitSendTime = time() - ($timeout * 60);
+
+			if (!isset($_POST['timestamp']) || $earliestLegitSendTime > $_POST['timestamp']) {
+					return 0;
+			}
+
+			$requestor = new Staff;
+			$rows = $requestor->getMatches(['username' => $_POST['username']]);
+
+			foreach ($rows as $row) {
+					$expected_hash = hash_hmac('md5', $_POST['mode'] . '-' . $row['username'] . '-' . $_POST['timestamp'], $row['secret_key']);
+					return ($expected_hash === $token); // return immediately on match
+			}
+
+			return 0; // no match found
+	}
 
 	public static function get_headers() {
 		if (!function_exists('getallheaders'))  {
