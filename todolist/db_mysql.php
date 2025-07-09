a7001f6118deeb0750419df47b87b18f5dd95528
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-09 16:33:20 +0800

    refactor: noauth.php, typo db_mysql.php fixed.

--- todolist/db_mysql.php
+++ todolist/db_mysql.php
@@ -2,17 +2,16 @@
 		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
 		 * See the file COPYRIGHT.html for more details. --F.Tumulak
 		 */
-require_once("../shared/guard_token.php");
-verify_token_or_die('guard_token_key');
+require_once("../catalog/class/Qtest.php");
 
-require_once("../todolist/db_mysql.php");
+$mypass = new Qtest;
+$connection = mysqli_connect(
+    $mypass->getDSN2("host"),
+    $mypass->getDSN2("username"),
+    $mypass->getDSN2("pwd"),
+    $mypass->getDSN2("database")
+);
 
-$id = (int)($_POST['id'] ?? 0);
-$done = isset($_POST['done']) && $_POST['done'] === "true" ? 1 : 0;
-
-if ($id > 0) {
-    $sql = "UPDATE todos SET is_done = $done WHERE id = $id";
-    mysqli_query($connection, $sql);
+if (mysqli_connect_errno()) {
+    die("MySQL connection failed: " . mysqli_connect_error());
 }
-
-include '../todolist/todo_list.php';
