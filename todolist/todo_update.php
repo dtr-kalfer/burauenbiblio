d5c0fcf8c89b117ef9c7bbf6c011c0fb6716af26
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-09 16:10:50 +0800

    refactor: todolist/todo_update.php, prepare for mysqli migration from sqlite.

--- todolist/todo_update.php
+++ todolist/todo_update.php
@@ -2,19 +2,17 @@
 		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
 		 * See the file COPYRIGHT.html for more details. --F.Tumulak
 		 */
+require_once("../shared/guard_token.php");
+verify_token_or_die('guard_token_key');
 
+require_once("../todolist/db_mysql.php");
 
-require '../todolist/db.php';
+$id = (int)($_POST['id'] ?? 0);
+$done = isset($_POST['done']) && $_POST['done'] === "true" ? 1 : 0;
 
-$id = $_POST['id'] ?? null;
-$done = $_POST['done'] ?? null;
-
-if (isset($id, $done)) {
-    $stmt = $db->prepare("UPDATE todos SET is_done = :done WHERE id = :id");
-    $stmt->execute([
-        ':done' => filter_var($done, FILTER_VALIDATE_BOOLEAN) ? 1 : 0,
-        ':id' => $id
-    ]);
+if ($id > 0) {
+    $sql = "UPDATE todos SET is_done = $done WHERE id = $id";
+    mysqli_query($connection, $sql);
 }
 
 include '../todolist/todo_list.php';
