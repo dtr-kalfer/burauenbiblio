574a654e701466ffde974428b84a15f85b53e733
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-07-09 16:13:49 +0800

    refactor: db.php saved into db_mysql.php to support mysqli migration from sqlite.

---
+++ todolist/db_mysql.php
@@ -0,0 +1,18 @@
+<?php
+		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+		 * See the file COPYRIGHT.html for more details. --F.Tumulak
+		 */
+require_once("../shared/guard_token.php");
+verify_token_or_die('guard_token_key');
+
+require_once("../todolist/db_mysql.php");
+
+$id = (int)($_POST['id'] ?? 0);
+$done = isset($_POST['done']) && $_POST['done'] === "true" ? 1 : 0;
+
+if ($id > 0) {
+    $sql = "UPDATE todos SET is_done = $done WHERE id = $id";
+    mysqli_query($connection, $sql);
+}
+
+include '../todolist/todo_list.php';
