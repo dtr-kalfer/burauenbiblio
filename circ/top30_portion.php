fdaa0cd5ab0b537b38930da0c8d8b3028c6a1fd8
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-08-08 21:12:17 +0800

    refactor: circ/top30_portion.php include isbn+author

--- circ/top30_portion.php
+++ circ/top30_portion.php
@@ -2,21 +2,28 @@
 /* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
  * See the file COPYRIGHT.html for more details.
  */
-		if (!isset($_SESSION['guard_token_key'])) {
-			  http_response_code(403);
-        echo "⛔ Access denied!";
-        exit;;
-		} 
+		require_once("../shared/guard_doggy.php");
+		
 	  // Assuming $pdo is your PDO connection and this query was prepared
     $stmt = $pdo->query("
 			SELECT 
 				titles.subfield_data AS title,
+				authors.subfield_data AS author,
+				isbns.subfield_data AS isbn,
 				COUNT(*) AS checkout_count
 			FROM booking b
-			JOIN biblio_field bf ON bf.bibid = b.bibid AND bf.tag = '245'
-			JOIN biblio_subfield titles ON titles.fieldid = bf.fieldid AND titles.subfield_cd = 'a'
+			JOIN biblio_field bf_title ON bf_title.bibid = b.bibid AND bf_title.tag = '245'
+			JOIN biblio_subfield titles ON titles.fieldid = bf_title.fieldid AND titles.subfield_cd = 'a'
+
+			LEFT JOIN biblio_field bf_author ON bf_author.bibid = b.bibid AND bf_author.tag = '100'
+			LEFT JOIN biblio_subfield authors ON authors.fieldid = bf_author.fieldid AND authors.subfield_cd = 'a'
+
+			LEFT JOIN biblio_field bf_isbn ON bf_isbn.bibid = b.bibid AND bf_isbn.tag = '020'
+			LEFT JOIN biblio_subfield isbns ON isbns.fieldid = bf_isbn.fieldid AND isbns.subfield_cd = 'a'
+
 			WHERE b.out_dt BETWEEN DATE_SUB(CURDATE(), INTERVAL 6 MONTH) AND CURDATE()
-			GROUP BY titles.subfield_data
+
+			GROUP BY titles.subfield_data, authors.subfield_data, isbns.subfield_data
 			ORDER BY checkout_count DESC
 			LIMIT 30;
     ");
@@ -25,7 +32,9 @@
     foreach ($stmt as $row): ?>
       <tr>
         <td><?= $rank++ ?></td>
-        <td class="topbooks"><?= htmlspecialchars($row['title']) ?></td>
+        <td><?= htmlspecialchars($row['title']) ?></td>
+				<td><?= htmlspecialchars($row['author']) ?></td>
+				<td><?= htmlspecialchars($row['isbn']) ?></td>
         <td><?= $row['checkout_count'] ?></td>
       </tr>
 	<?php endforeach; ?>
