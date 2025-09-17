a5a87b47f72fcc0616e89415fbe85c34c4412b2d
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-09-17 13:03:22 +0800

    refactor: circ/top30.php use class and objects

--- circ/top30.php
+++ circ/top30.php
@@ -11,21 +11,11 @@ require_once(REL(__FILE__, "../shared/logincheck.php"));
 
 Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
 
-//require_once("top30_function.php"); 
-require_once("../catalog/class/Qtest.php");
-
-$mypass = new Qtest;
-
-try {
-    $dsn = 'mysql:host=' . $mypass->getDSN2("host") . ';dbname=' . $mypass->getDSN2("database") . ';charset=utf8mb4';
-    $pdo = new PDO($dsn, $mypass->getDSN2("username"), $mypass->getDSN2("pwd"));
-
-    // Enable exception mode
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    die("PDO Connection failed: " . $e->getMessage());
-}
+		require_once __DIR__ . '/../autoload.php'; // adjust the ../ if necessary depending on your source path.
+		use BookUtilization\Top30borrow;
+		
+		$result = new Top30borrow();
+		$stmt = $result->make_top30borrowlist();
 
 // Get current date
 date_default_timezone_set('Asia/Manila');
