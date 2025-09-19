a9cd80286532a83de57fc6fe47f3da284ec95d36
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-09-19 21:45:27 +0800

    update: admin/migration_process.php migrate MyISAM to InnoDB and fix zero-date value by using CURRENT_TIMESTAMP on altering some table.

--- admin/migration_process.php
+++ admin/migration_process.php
@@ -39,12 +39,6 @@ $db->query("
     )
 ");
 
-// Define migrations 
-//"sql"  => "ALTER TABLE mbr_classify_dm ADD loan_allotment SMALLINT(3) NOT NULL DEFAULT 0"
-//"sql"  => "ALTER TABLE mbr_classify_dm MODIFY max_fines DECIMAL(6,2) NOT NULL"
-//"sql"  => "ALTER TABLE member_account MODIFY create_dt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP"
-//"sql"  => "ALTER TABLE member_account DROP PRIMARY KEY, ADD PRIMARY KEY (transid), MODIFY transid INT(11) NOT NULL AUTO_INCREMENT"
-
 $migrations = [
     [
         "name" => "Add_loan_allotment_feature",
@@ -96,7 +90,7 @@ $migrations = [
     ],
     [
         "name" => "Create_table_courses",
-        "sql"  => "CREATE TABLE tbl_courses (
+        "sql"  => "CREATE TABLE IF NOT EXISTS tbl_courses (
 						id INT AUTO_INCREMENT PRIMARY KEY,
 						course_name VARCHAR(100) NOT NULL UNIQUE
         )"
@@ -110,7 +104,230 @@ $migrations = [
 					('BSE'),
 					('BMLS')
         "
-    ],				
+    ],
+				// Fix error zero-date defaults (0000-00-00 00:00:00) which errors on InnoDB engine
+    [
+        "name" => "Fix error: invalid datetime defaults biblio_copy InnoDB migrate",
+				"sql"  => "ALTER TABLE `biblio_copy`
+										MODIFY `create_dt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+										MODIFY `last_change_dt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+										ON UPDATE CURRENT_TIMESTAMP		
+									"		
+    ],
+    [
+        "name" => "Fix error: invalid datetime defaults biblio InnoDB migrate",
+				"sql"  => "ALTER TABLE `biblio`
+										MODIFY `create_dt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+										MODIFY `last_change_dt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+										ON UPDATE CURRENT_TIMESTAMP	
+									"		
+    ],
+
+		// Make sure all create tables are set before updating the engine
+		
+		// Update openbiblio table engine to InnoDB
+    [
+        "name" => "Change to InnoDB table `usmarc_indicator_dm`",
+        "sql"  => "ALTER TABLE `usmarc_indicator_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `cutter`",
+        "sql"  => "ALTER TABLE `cutter` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `member_account`",
+        "sql"  => "ALTER TABLE `member_account` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `lookup_settings`",
+        "sql"  => "ALTER TABLE `lookup_settings` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `open_hours`",
+        "sql"  => "ALTER TABLE `open_hours` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `transsections`",
+        "sql"  => "ALTER TABLE `transsections` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `material_fields`",
+        "sql"  => "ALTER TABLE `material_fields` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `validation_dm`",
+        "sql"  => "ALTER TABLE `validation_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `report_displays`",
+        "sql"  => "ALTER TABLE `report_displays` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `state_dm`",
+        "sql"  => "ALTER TABLE `state_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `translinksectionlocale`",
+        "sql"  => "ALTER TABLE `translinksectionlocale` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `session`",
+        "sql"  => "ALTER TABLE `session` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `calendar_dm`",
+        "sql"  => "ALTER TABLE `calendar_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `transkeys`",
+        "sql"  => "ALTER TABLE `transkeys` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_status_dm`",
+        "sql"  => "ALTER TABLE `biblio_status_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `usmarc_block_dm`",
+        "sql"  => "ALTER TABLE `usmarc_block_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `mbr_classify_dm`",
+        "sql"  => "ALTER TABLE `mbr_classify_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `cart`",
+        "sql"  => "ALTER TABLE `cart` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `view_fields`",
+        "sql"  => "ALTER TABLE `view_fields` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_hold`",
+        "sql"  => "ALTER TABLE `biblio_hold` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `collection_dist`",
+        "sql"  => "ALTER TABLE `collection_dist` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `translocales`",
+        "sql"  => "ALTER TABLE `translocales` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_subfield`",
+        "sql"  => "ALTER TABLE `biblio_subfield` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `usmarc_subfield_dm`",
+        "sql"  => "ALTER TABLE `usmarc_subfield_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `member_fields`",
+        "sql"  => "ALTER TABLE `member_fields` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `php_sess`",
+        "sql"  => "ALTER TABLE `php_sess` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_copy_fields_dm`",
+        "sql"  => "ALTER TABLE `biblio_copy_fields_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_status_hist`",
+        "sql"  => "ALTER TABLE `biblio_status_hist` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `usmarc_tag_dm`",
+        "sql"  => "ALTER TABLE `usmarc_tag_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `material_type_dm`",
+        "sql"  => "ALTER TABLE `material_type_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `transaction_type_dm`",
+        "sql"  => "ALTER TABLE `transaction_type_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_copy_fields`",
+        "sql"  => "ALTER TABLE `biblio_copy_fields` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `site`",
+        "sql"  => "ALTER TABLE `site` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `images`",
+        "sql"  => "ALTER TABLE `images` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `member_fields_dm`",
+        "sql"  => "ALTER TABLE `member_fields_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `booking`",
+        "sql"  => "ALTER TABLE `booking` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `staff`",
+        "sql"  => "ALTER TABLE `staff` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `calendar`",
+        "sql"  => "ALTER TABLE `calendar` ENGINE=InnoDB"
+    ],
+
+    [
+        "name" => "Change to InnoDB table `booking_member`",
+        "sql"  => "ALTER TABLE `booking_member` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `lookup_hosts`",
+        "sql"  => "ALTER TABLE `lookup_hosts` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `transentries`",
+        "sql"  => "ALTER TABLE `transentries` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_stock`",
+        "sql"  => "ALTER TABLE `biblio_stock` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `collection_circ`",
+        "sql"  => "ALTER TABLE `collection_circ` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `member`",
+        "sql"  => "ALTER TABLE `member` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `biblio_field`",
+        "sql"  => "ALTER TABLE `biblio_field` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `collection_dm`",
+        "sql"  => "ALTER TABLE `collection_dm` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `theme`",
+        "sql"  => "ALTER TABLE `theme` ENGINE=InnoDB"
+    ],
+    [
+        "name" => "Change to InnoDB table `settings`",
+        "sql"  => "ALTER TABLE `settings` ENGINE=InnoDB"
+    ],
+		[
+        "name" => "Change to InnoDB table `biblio`",
+        "sql"  => "ALTER TABLE `biblio` ENGINE=InnoDB"
+    ],
+		[
+        "name" => "Change to InnoDB table `biblio_copy`",
+        "sql"  => "ALTER TABLE `biblio_copy` ENGINE=InnoDB"
+    ],
+		
     // Add more future migrations here...
 ];
 
