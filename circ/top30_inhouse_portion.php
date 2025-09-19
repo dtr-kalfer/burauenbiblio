4b72db17d638395c398a76d78ae52e1ba76e70df
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-09-19 09:21:51 +0800

    update: circ/top30_inhouse_portion.php

--- circ/top30_inhouse_portion.php
+++ circ/top30_inhouse_portion.php
@@ -1,19 +1,28 @@
-<?php
-/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
- * See the file COPYRIGHT.html for more details.
- */
+	<?php
+	/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+	 * See the file COPYRIGHT.html for more details.
+	 */
 		require_once("../shared/guard_doggy.php");
-
-    $rank = 1;
-    foreach ($stmt as $row): ?>
-      <tr>
-        <td><?= $rank++ ?></td>
-        <td><?= htmlspecialchars($row['title']) ?></td>
-				<td><?= htmlspecialchars($row['author']) ?></td>
-				<td><?= htmlspecialchars($row['isbn']) ?></td>
-        <td><?= $row['view_count'] ?></td>
-      </tr>
-	<?php endforeach; ?>
+		
+		if ($stmt['success'] === true) {
+		?>
+			<p style="text-align: center;"><?= $stmt['message'] ?></p>
+		<?php
+			$rank = 1;
+			foreach ($stmt['content'] as $row): 
+	?>
+		<tr>
+			<td><?= $rank++ ?></td>
+			<td><?= htmlspecialchars($row['title']) ?></td>
+			<td><?= htmlspecialchars($row['author']) ?></td>
+			<td><?= htmlspecialchars($row['isbn']) ?></td>
+			<td><?= $row['view_count'] ?></td>
+		</tr>
+	<?php endforeach; 
+		} else {
+			echo $stmt['message'];
+		}
+	?>
 
   </tbody>
 </table>
