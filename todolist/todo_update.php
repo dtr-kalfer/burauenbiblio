988481ee92469c3cb80648a70e0f464490bab451
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-09-25 21:28:21 +0800

    refactor: todolist/todo_update.php

--- todolist/todo_update.php
+++ todolist/todo_update.php
@@ -2,17 +2,18 @@
 		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
 		 * See the file COPYRIGHT.html for more details. --F.Tumulak
 		 */
-require_once("../shared/guard_token.php");
-verify_token_or_die('guard_token_key');
+require_once("../shared/guard_doggy.php");
+require_once __DIR__ . '/../autoload.php'; // adjust the ../ if necessary depending on your source path.
 
-require_once("../todolist/db_mysql.php");
+use  Todo_List\Todo_List;
 
-$id = (int)($_POST['id'] ?? 0);
-$done = isset($_POST['done']) && $_POST['done'] === "true" ? 1 : 0;
+$todo = new Todo_List();
+
+$id   = (int)($_POST['id'] ?? 0);
+$done = isset($_POST['done']) && $_POST['done'] === "true";
 
 if ($id > 0) {
-    $sql = "UPDATE todos SET is_done = $done WHERE id = $id";
-    mysqli_query($connection, $sql);
+    $todo->updateTask($id, $done);
 }
 
 include '../todolist/todo_list.php';
