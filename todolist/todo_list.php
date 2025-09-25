c5d4bcfd3cd8ea8a50e906b8d218be2f709e2e5a
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-09-25 21:26:01 +0800

    refactor: todolist/todo_list.php

--- todolist/todo_list.php
+++ todolist/todo_list.php
@@ -2,38 +2,32 @@
 		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
 		 * See the file COPYRIGHT.html for more details. --F.Tumulak
 		 */
-require_once("../shared/guard_token.php");
-verify_token_or_die('guard_token_key');
+require_once("../shared/guard_doggy.php");
+require_once __DIR__ . '/../autoload.php'; // adjust the ../ if necessary depending on your source path.
 
-require_once("../todolist/db_mysql.php");
+use  Todo_List\Todo_List;
 
-$sql = "SELECT * FROM todos ORDER BY id ASC";
-$result = mysqli_query($connection, $sql);
+$todo   = new Todo_List();
 
-while ($todo = mysqli_fetch_assoc($result)): ?>
-  <li class="<?= $todo['is_done'] ? 'done' : '' ?>">
+$tasks = $todo->getAllTasks();
+
+foreach ($tasks as $t): ?>
+  <li class="<?= $t['is_done'] ? 'done' : '' ?>">
     <input type="checkbox"
-      <?= $todo['is_done'] ? 'checked' : '' ?>
+      <?= $t['is_done'] ? 'checked' : '' ?>
       hx-post="../todolist/todo_update.php"
-      hx-vals='{
-        "id": <?= $todo['id'] ?>,
-        "done": <?= $todo['is_done'] ? 'false' : 'true' ?>,
-        "guard_token_key": "<?= $_SESSION['guard_token_key'] ?? $_GET['guard_token_key'] ?>"
-      }'
+      hx-vals='{"id": <?= $t['id'] ?>, "done": <?= $t['is_done'] ? 'false' : 'true' ?>}'
       hx-target="#todo-list"
       hx-swap="innerHTML"
-			hx-disabled-elt="checkbox"
+      hx-disabled-elt="checkbox"
     >
-    <?= htmlspecialchars($todo['task']) ?>
+    <?= htmlspecialchars($t['task']) ?>
     <button
       hx-post="../todolist/todo_delete.php"
-      hx-vals='{
-        "id": <?= $todo['id'] ?>,
-        "guard_token_key": "<?= $_SESSION['guard_token_key'] ?? $_GET['guard_token_key'] ?>"
-      }'
+      hx-vals='{"id": <?= $t['id'] ?>}'
       hx-target="#todo-list"
       hx-swap="innerHTML"
-			hx-disabled-elt="button"
+      hx-disabled-elt="button"
     >🗑</button>
   </li>
-<?php endwhile; ?>
+<?php endforeach; ?>
