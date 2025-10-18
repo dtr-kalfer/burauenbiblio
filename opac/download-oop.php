f1e727bb5a7ae081a9a184f8b3cdbda68f33d291
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-10-18 21:15:18 +0800

    refactor: opac/download-oop.php refactor into object-class setup.

---
+++ opac/download-oop.php
@@ -0,0 +1,31 @@
+<?php
+require_once(__DIR__ . "/../classes/GutendexClient.php");
+
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if (!$id) die("Missing book ID.");
+
+$client = new GutendexClient();
+$book = $client->getBook($id);
+
+if (!$book) die("Error fetching book info.");
+
+$formats = $book['formats'] ?? [];
+$preferred = $client->getPreferredDownload($formats);
+
+if (!$preferred) {
+    echo "Format not available.<br><pre>";
+    print_r(array_keys($formats));
+    echo "</pre>";
+    exit;
+}
+
+$title = $book['title'] ?? "GutenbergBook";
+$title = $client->sanitizeFilename($title, $id);
+$filename = "{$title}.{$preferred['ext']}";
+$url = $preferred['url'];
+
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
+readfile($url);
+exit;
+?>
