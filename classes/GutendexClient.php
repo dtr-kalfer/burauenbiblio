8ff7399f262ba377ecb3775f869f4f415652e07e
Ferdinand Tumulak <mymikrotiki@gmail.com>
2025-10-18 21:37:24 +0800

    update: classes/GutendexClient.php

--- classes/GutendexClient.php
+++ classes/GutendexClient.php
@@ -1,4 +1,7 @@
 <?php
+		/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+		 * See the file COPYRIGHT.html for more details. --F.Tumulak
+		 */
 // classes/GutendexClient.php
 class GutendexClient {
     private string $apiBase = "https://gutendex.com/books/";
