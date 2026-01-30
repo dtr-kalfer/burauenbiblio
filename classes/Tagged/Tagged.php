f95e677895d3198cab94b3418495ab1125a106d2
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-01-30 10:28:51 +0800

    update: class/Tagged/Tagged.php refactored sql commands to use MARC tagging instead of fields+x value

--- classes/Tagged/Tagged.php
+++ classes/Tagged/Tagged.php
@@ -1,5 +1,8 @@
 <?php
-
+/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+ * See the file COPYRIGHT.html for more details. --F. Tumulak 1/30/2026
+ */
+ 
 namespace Tagged;
 
 class Tagged extends \ConnectDB
@@ -12,47 +15,46 @@ class Tagged extends \ConnectDB
     public function getTaggedBibItems(): array
     {
         $sql = "
-            SELECT 
-                bc.bibid,
-                title_field.field_entry AS title,
-                author_field.field_entry AS author
-            FROM (
-                SELECT id AS bibid
-                FROM cart
-            ) c
-            JOIN (
-                SELECT *
-                FROM biblio_copy bc1
-                WHERE (bc1.bibid, bc1.copyid) IN (
-                    SELECT bibid, MIN(copyid)
-                    FROM biblio_copy
-                    GROUP BY bibid
-                )
-            ) bc ON c.bibid = bc.bibid
-            LEFT JOIN (
-                SELECT 
-                    bibid,
-                    GROUP_CONCAT(subfield_data ORDER BY subfield_data SEPARATOR ',') AS field_entry
-                FROM biblio_subfield
-                WHERE (bibid, fieldid) IN (
-                    SELECT bibid, MIN(fieldid) + 2
-                    FROM biblio_subfield
-                    GROUP BY bibid
-                )
-                GROUP BY bibid
-            ) title_field ON bc.bibid = title_field.bibid
-            LEFT JOIN (
-                SELECT 
-                    bibid,
-                    GROUP_CONCAT(subfield_data ORDER BY subfield_data SEPARATOR ',') AS field_entry
-                FROM biblio_subfield
-                WHERE (bibid, fieldid) IN (
-                    SELECT bibid, MIN(fieldid) + 1
-                    FROM biblio_subfield
-                    GROUP BY bibid
-                )
-                GROUP BY bibid
-            ) author_field ON bc.bibid = author_field.bibid
+				SELECT
+						c.id AS bibid,
+						cn.subfield_data AS call_number,
+						CONCAT(
+								t.subfield_data,
+								IFNULL(CONCAT(' ', tb.subfield_data), '')
+						) AS title,
+						a.subfield_data AS author
+
+				FROM cart c
+
+				LEFT JOIN biblio_field cf
+						ON cf.bibid = c.id
+					 AND cf.tag = '099'
+
+				LEFT JOIN biblio_subfield cn
+						ON cn.fieldid = cf.fieldid
+					 AND cn.subfield_cd = 'a'
+
+				JOIN biblio_field tf
+						ON tf.bibid = c.id
+					 AND tf.tag = '245'
+
+				JOIN biblio_subfield t
+						ON t.fieldid = tf.fieldid
+					 AND t.subfield_cd = 'a'
+
+				LEFT JOIN biblio_subfield tb
+						ON tb.fieldid = tf.fieldid
+					 AND tb.subfield_cd = 'b'
+
+				LEFT JOIN biblio_field af
+						ON af.bibid = c.id
+					 AND af.tag = '100'
+
+				LEFT JOIN biblio_subfield a
+						ON a.fieldid = af.fieldid
+					 AND a.subfield_cd = 'a'
+
+				ORDER BY title;
         ";
 
         // Uses ConnectDB::select()
