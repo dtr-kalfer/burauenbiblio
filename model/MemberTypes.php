6efe54c7cc27fc5a031dacc5081e6702e67c09da
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-08-04 19:37:11 +0800

    refactor: model/MemberTypes.php to use DmTable::validate_el() checks for duplicate already.

--- model/MemberTypes.php
+++ model/MemberTypes.php
@@ -6,46 +6,43 @@
 require_once(REL(__FILE__, "../classes/DmTable.php"));
 
 class MemberTypes extends DmTable {
-	public function __construct() {
-		parent::__construct();
-		$this->setName('mbr_classify_dm');
-		$this->setFields(array(
-			'code'=>'string',
-			'description'=>'string',
-			'default_flg'=>'string',
-			'max_fines'=>'number',
-			'loan_allotment'=>'number',
-		));
+  public function __construct() {
+    parent::__construct();
+    $this->setName('mbr_classify_dm');
+    $this->setFields(array(
+      'code'=>'string',
+      'description'=>'string',
+      'default_flg'=>'string',
+      'max_fines'=>'number',
+      'loan_allotment'=>'number',
+    ));
         $this->setReq(array(
             'description', 'default_flg', 'max_fines', 'loan_allotment',
         ));
-		$this->setKey('code');
-	}
+    $this->setKey('code');
+  }
 
-	public function insert($rec, $confirmed=false) {
+  public function insert($rec, $confirmed=false) {
         // if no default flg present, set to 'N'
-		if (!isset($rec['default_flg'])) {
+    if (!isset($rec['default_flg'])) {
             $rec['default_flg'] = 'N';
         }
         list($parm1, $parm2) = parent::insert($rec, $confirmed=false);
         return array($parm1, $parm2);
     }
 
-	protected function validate_el($rec, $insert) {
-		// check for required fields done in DBTable
-		$errors = parent::validate_el($rec, $insert);
-        // duplicate mbrType codes not allowed
-		$sql = $this->mkSQL("SELECT * FROM %q WHERE code=%Q ", $this->name, $rec['code']);
-		$rslt = $this->select($sql);
-        $rows = $rslt->fetchAll();
-        if ($insert&& (count($rows) != 0)) {
-			//$errors[] = new FieldError('code', T("Duplicate Code not allowed"));
-			$errors[] = T("Duplicate Code not allowed");
-		}
-        // otherwise limit default flg to Y or N only
-        if ($rec['default_flg'] != 'Y' && $rec['default_flg']!= 'N') {
-			$errors[] = new FieldError('default_flg', T("Default Flg MUST be 'Y' or 'N'"));
-        }
-		return $errors;
-	}
+  protected function validate_el($rec, $insert) {
+    // check for required fields done in DBTable
+    // DmTable::validate_el() already checks for duplicate codes and default_flg
+    $errors = parent::validate_el($rec, $insert);
+
+    // validate max_fines and loan_allotment are numeric
+    foreach (['max_fines', 'loan_allotment'] as $f) {
+      if (isset($rec[$f]) && $rec[$f] !== '' && !is_numeric($rec[$f])) {
+        $errors[] = new FieldError($f, T("Field must be numeric"));
+      }
+    }
+
+    return $errors;
+  }
 }
