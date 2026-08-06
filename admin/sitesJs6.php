4689ee27b9448dc736c4ada3cb537ccc56506625
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-08-06 21:26:00 +0800

    update and refactor: admin/sitesJs6.php, Removed country from constructor, removed await list.getPullDownList() from fetchList(),

--- admin/sitesJs6.php
+++ admin/sitesJs6.php
@@ -1,132 +1,106 @@
-<script language="JavaScript" >
+<script>
 /* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
  * See the file COPYRIGHT.html for more details.
+ *
+ * Refactored for single-site deployment (PHP 8.3).
+ * "Add New" and "Merge Sites" removed — only Update/Delete remain.
  */
-// JavaScript Document
 "use strict";
 
 class Sit extends Admin {
     constructor () {
-    	var url = '../admin/adminSrvr.php',
-    		form = $('#editForm'),
-    		dbAlias = 'sites';
-    	var hdrs = {'listHdr':<?php echo '"'.T("List of Sites").'"'; ?>,
-    				'editHdr':<?php echo '"'.T("Edit Site").'"'; ?>,
-    				'newHdr':<?php echo '"'.T("Add New Site").'"'; ?>,
-    				};
-    	var listFlds = {'name': 'text',
-    					'code': 'text',
-    					'city':'text',
-    				   };
-    	var opts = { 'focusFld':'name', 'keyFld':'siteid' };
+      var url = '../admin/adminSrvr.php',
+        form = $('#editForm'),
+        dbAlias = 'sites';
+      var hdrs = {'listHdr':<?php echo '"'.T("List of Sites").'"'; ?>,
+            'editHdr':<?php echo '"'.T("Edit Site").'"'; ?>,
+            'newHdr':<?php echo '"'.T("Add New Site").'"'; ?>,
+            };
+      var listFlds = {'name': 'text',
+              'code': 'text',
+              'city':'text',
+               };
+      var opts = { 'focusFld':'name', 'keyFld':'siteid' };
 
-	    super( url, form, dbAlias, hdrs, listFlds, opts );
+      super( url, form, dbAlias, hdrs, listFlds, opts );
 
-    	this.noshows = [];
+      this.noshows = [];
 
-		$('#mergeBtn').on('click',null,this.doMergeSites);
-		//list.getSiteList($('#fmSite'));
-		//list.getSiteList($('#toSite'));
-        //list.getPullDownList('State', $('#state'));
-        //list.getPullDownList('Calendar', $('#calendar'));
-
-        $('#country').val('xxxx');
     };
 
-	async fetchList() {
-		// using 'promise' technique to insure calls are processed in-turn
-		await list.getSiteHoldings();
-		await super.fetchList();
+  async fetchList() {
+    // using 'promise' technique to insure calls are processed in-turn
+    await list.getSiteHoldings();
+    await super.fetchList();
 
-		await list.getSiteList($('#fmSite'));
-		await list.getSiteList($('#toSite'));
-		await list.getPullDownList('State', $('#state'));
-		await list.getPullDownList('Calendar', $('#calendar'));
-	};
+    await list.getPullDownList('Calendar', $('#calendar'));
+  };
+
     fetchHandler (dataAray) {
-		super.fetchHandler(dataAray);
-		//this.holdings = list.holdings
-		var $rows = $('#showList tbody tr');
+    super.fetchHandler(dataAray);
 
-		// add holdings to each site display
-		$rows.each(function (i){
-			var siteid = $(this).find('input[type="hidden"]').val();
-			let nmbr = list.holdings[siteid];
-			let html = '<td>'+nmbr+'</td>';
-			$(this).append(html);
-		});
-	};
+    var $rows = $('#showList tbody tr');
 
-    //fetchStates () {
-    //};
+    // add holdings to each site display
+    $rows.each(function (i){
+      var siteid = $(this).find('input[type="hidden"]').val();
+      let nmbr = list.holdings[siteid];
+      let html = '<td>'+nmbr+'</td>';
+      $(this).append(html);
+    });
+  };
 
-    //fetchCalendars () {
-    //};
+    // ---- Override: disable Add New completely ----
+    doNewFields () {
+    // no-op: single-site mode does not allow adding new sites
+    return false;
+    };
 
-	doMergeSites(e) {
-    	$('#listDiv').hide();
-    	$('#extraDiv').show();
-		$('#mergeSiteBtn').enable();
-		$('#fmSite').on('change', null, function () {
-			let nmbr = list.holdings[$('#fmSite').val()];
-			$('#limit').val(nmbr);
-		});
-	};
-
+    // ---- Override: remove Add button from submit handling ----
     doSubmitFields (e) {
-    	var theBtn = e.target.id;
-    	if (theBtn == 'mergeSiteBtn') {
-			let fm = $('#fmSite').val();
-			let to = $('#toSite').val();
-			let maxHit = list.holdings[fm];
-			let limit = $('#limit').val();
-			if (this.validateMerge(fm, to, limit, maxHit)) {
-    			e.preventDefault();
-    			e.stopPropagation();
-				$('#mergeSiteBtn').disable();
-				this.merge(fm, to, limit);
-			} else
-				return false;
-		} else {
-			super.doSubmitFields(e);
-		}
-	};
-	validateMerge(fm, to, limit, maxHit){
-		let crntSite = <?php echo $_SESSION['current_site']; ?>;
-		if (fm == crntSite) {
-			alert('You may not remove items from logged-in site');
-			return false;
-		} else if (fm == to) {
-			alert('Both sites cannot be the same');
-			return false;
-		} else if (limit > maxHit) {
-			alert('Number to transfer cannot exceed holdings.');
-		}
-		return true;
-	};
-	merge(fm, to, limit) {
-		let parms = "cat=sites&mode=mergeSites&fm="+fm+"&to="+to+"&limit="+limit;
-    	//$.post(this.url, parms, $.proxy(this.mergeHandler, this), 'json');
-    	$.post(this.url, parms, this.mergeHandler, 'json');
-	};
-	mergeHandler(response) {
-		//console.log(response);
-		super.showResponse(response);
-	};
+      var theBtn = e.target.id;
 
-    doNewFields (e) {
-        var localeCntry = navigator.language.slice(-2);
-        //console.log("country is "+localeCntry);
+      if (theBtn == 'addBtn') {
+        // silently ignore — Add button no longer exists in the DOM
+        return false;
+      }
 
-        super.doNewFields.apply(this);
-        //console.log('got here');
-        $('#country').val(localeCntry);
-    	$('#editDiv').show();
-    }
+      super.doSubmitFields(e);
+    };
+
+    // ---- Override: hide Add/Delete in edit mode (single-site: keep one site) ----
+    showFields (rec) {
+        $('#fieldsHdr').html(this.editHdr);
+        $('#addBtn').hide();
+        $('#updtBtn').show().enable();
+        $('#deltBtn').show().enable();
+
+      $('#editTbl').find('input:not(:button):not(:submit):not(:password), textarea, select').each(function () {
+      var $this = $(this);
+
+        if ($this.is('[type=radio]')) {
+          $this.val([rec[this.name]]);
+        } else {
+        $this.val([rec[this.id]]);
+        }
+
+            var theClass = $this.get(0).className;
+            if (theClass == 'addOnly') {
+                $this.attr('readOnly',true);
+            }
+      });
+
+      for (var n in this.noshows){
+        $('#'+this.noshows[n]).attr('required',false).hide();
+      };
+
+      $('#codeReqd').hide();
+      $('#listDiv').hide();
+      $('#editDiv').show();
+    };
 }
 
 $(document).ready(function () {
-	var xxxx = new Sit();
+  var xxxx = new Sit();
 });
-
 </script>
