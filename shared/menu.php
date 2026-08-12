214ed8b6728df1cc7bc87737bc8f9d755dda12e9
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-08-12 22:25:53 +0800

    updated: shared/menu.php updated new node for Missing Books Report

--- shared/menu.php
+++ shared/menu.php
@@ -39,6 +39,7 @@ function staff_menu() {
 			Nav::node('circulation/analytics/top30', T("top30"),'../circ/top30.php');
 			Nav::node('circulation/analytics/top30_inhouse', T("top30_in-house"),'../circ/top30_inhouse.php');
 			Nav::node('circulation/analytics/dailytally', T("Daily T. (Chart)"),'../circ/dailytallychart.php');
+			Nav::node('circulation/analytics/missingbooks', T("Missing Books"),'../circ/missing_books_report.php');
 	}
 
 	##-------------------------------------------------------------------------------------##
