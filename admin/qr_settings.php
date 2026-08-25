a4001bdb8b0d8d6d53b23b88f1710553df6fe85c
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-08-25 18:39:16 +0800

    update: admin/qr_settings.php updated member qr print to accomodate 48 individual prints without additional duplicates to save paper (optional feature).

--- admin/qr_settings.php
+++ admin/qr_settings.php
@@ -3,11 +3,11 @@
  * See the file COPYRIGHT.html for more details. --F. Tumulak 1/30/2026
  */
 require_once("../shared/guard_doggy.php");
- 
+
 $qrSize     = 140;
 $textArea  = 35;
 $imgWidth  = $qrSize;
 $imgHeight = $qrSize + $textArea;
 $fontSize  = 13;
 
-$QR_MAX_IMAGES = 16;
+$QR_MAX_IMAGES = 48; // max unique images in batch folder (up to 48 for 1-copy/cell mode, 16 for 3-copies mode)
